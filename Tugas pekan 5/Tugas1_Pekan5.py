bccd9ee4ec61e07ada540971096a72e6da547790
Sitiriz <sitirzkyh01@gmail.com>
2023-11-05 22:58:43 +0700

    baru

---
+++ Tugas pekan 5/Tugas1_Pekan5.py
@@ -0,0 +1,7 @@
+kendaraan = ["Beat", "Mattic", "150cc", "Hitam", "2"]
+print(kendaraan)
+kendaraan.append("10 juta")
+kendaraan.append("sepeda motor")
+print(kendaraan)
+kendaraan.insert(2, "Honda")
+print(kendaraan)
