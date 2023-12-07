fe7f526f6c91885ff138823b2b0d0b821d7441b6
Sitiriz <sitirzkyh01@gmail.com>
2023-12-07 17:39:54 +0700

    tugas

---
+++ Tugas Pekan 11/hasil.py
@@ -0,0 +1,27 @@
+from bangun_datar import *
+import kalkulator
+
+persegi (5)
+
+persegi_panjang (5, 8)
+
+segitiga_sama_sisi (3, 5)
+
+jajar_genjang (3, 5, 7)
+
+lingkaran (8)
+
+belah_ketupat (2, 4, 6)
+
+layang_layang (2, 4, 6, 8)
+
+kalkulator.tambah(18, 2)
+kalkulator.kurang(18, 2)
+kalkulator.sin (18)
+kalkulator.pangkat (8,2)
+kalkulator.perkalian(18, 8)
+kalkulator.pembagian(60, 5)
+kalkulator.log(10)
+kalkulator.cos(20)
+kalkulator.tan(30)
+kalkulator.akar(18)
---
+++ Tugas Pekan 11/kalkulator.py
@@ -0,0 +1,44 @@
+import math
+
+# Penjumlahan
+def tambah(bil1, bil2):
+    hasil = bil1 + bil2
+    print('penjumlahan', bil1, "+", bil2, '=', hasil)
+
+# Pengurangan
+def kurang(bil1, bil2):
+    hasil = (bil2 - bil2 )
+
+# Pangkat, math.pow
+def pangkat(bilangan1, pangkat):
+    print('pangkat',bilangan1, '^', pangkat , 'adalah',math.pow(2,3))
+
+# Perkalian
+def perkalian(bil1, bil2):
+    hasil = (bil1 * bil2)
+    print('perkalian', bil1, '*', bil2, '=', hasil)
+
+# Pembagian
+def pembagian (bil1, bil2):
+    hasil =bil1 / bil2
+    print('pembagian', bil1, '/', bil2, '=', hasil)
+
+# Log, math.log
+def log (bil):
+    print('log dari', bil, 'adalah', math.log(bil))
+
+# Akar, math.sqrt
+def akar (bil):
+    print('akar dari', bil, 'adalah', math.log(bil))
+
+# Sin,
+def sin(bil):
+    print('sin dari', bil, 'adalah', math.sin(bil))
+
+# Cos,
+def cos(bil):
+    print('cos dari', bil, 'adalah', math.cos(bil))
+    
+# Tan,
+def tan (bil):
+    print('tan dari', bil, 'adalah', math.tan(bil))
